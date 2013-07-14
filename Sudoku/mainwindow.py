39ca4214dd452cc4c54228b460c542f28967c782
Manuel Suarez <man.srz@gmail.com>
2013-07-13 23:45:11 -0500

    UI

--- Sudoku/mainwindow.py
+++ Sudoku/mainwindow.py
@@ -1,23 +1,33 @@
-from PyQt4 import QtCore, QtGui
+try:
+    import  PySide
+    USE_PYSIDE = True
+except:
+    from PyQt4 import uic, QtGui, QtCore, phonon
+    PySide.QtCore.Signal = PySide.QtCore.pyqtSignal
+    PySide.QtCore.Slot = PySide.QtCore.pyqtSlot
+    USE_PYSIDE = False
 
-
-
-class MainWindow(QtGui.QMainWindow,QtGui.QWidget):
+class MainWindow(PySide.QtGui.QMainWindow,PySide.QtGui.QWidget):
 
     def __init__(self):
         super(MainWindow,self).__init__()
-        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        self.setAttribute(PySide.QtCore.Qt.WA_DeleteOnClose)
         self.setWindowTitle("SUDOKU")
+        try:
+           ### self.ui = QtUiTools.QUiLoader().load('../ui/mainwindow.ui')
+        except:
+            self.ui = uic.loadUi('../ui/mainwindow.ui')
+
         self.resize(400, 300)
 
-    
+
 
 
 
 
 if __name__ == '__main__':
     import sys
-    app = QtGui.QApplication(sys.argv)
+    app = PySide.QtGui.QApplication(sys.argv)
     mainWin = MainWindow()
     mainWin.show()
     sys.exit(app.exec_())
