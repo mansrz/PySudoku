07dcb106ab34811547a3b53eeb71cf543fbb379d
Manuel Suarez <man.srz@gmail.com>
2013-07-30 02:47:01 -0500

    Clase Generador Final
    Clase Generador
    Esta clase contiene 3 listas:
    -Tablero Generado(Unidimensional)
    -Tablero Genreado(Matriz)
    -Tablero de Visibilidad(Resultado de la dificultad)
    Se utiliza la tecnica de backtracking para generar Tablero
    Se implemento algoritmo de Dificultad basado en las posibilidades de
    celdas sacadas al azar

--- ui/Generador.py
+++ ui/Generador.py
@@ -1,200 +1,177 @@
 import random
+
+class Celda:
+
+    def __init__(self, fila, columna, text):
+        self.fila = fila
+        self.columna = columna
+        self.cuadricula = (fila // 3) * 3 + (columna // 3)
+        self.valor = 0
+        self.valorCorrecto = 0
+        self.text = text
+
+    def getValor(self):
+        return self.valor
+
+    def getValorCorrecto(self):
+        return self.valorCorrecto
+
+    def getCuadricula(self):
+        return self.cuadricula
+
+    def setText(self, text):
+        self.text = text
+
+    def setValor(self, valor):
+        self.valor = valor
+
+    def setValorCorrecto(self, valorCorrecto):
+        self.valorCorrecto = valorCorrecto
+
+
+
+
 class Generador:
+    celda = [[Celda(fila, columna, "") for columna in range(9)] for fila in range(9)]
+    tablero=[]*80
+    visibles= [ True for i in range(80)]
+    def __init__(self, dificultad):
 
-    def __init__(self):
-        self.tablero=[]*80
-        for i in range(9):
-            for j in range(9):
-             self.tablero.insert(i*9+j,0)
-        self.generarSolucionRandom()
         #for i in range (10):
+        while (not self.checkSudokuForZero()):
+            self.wipeSudokuClean()
+            self.sudokuGenerator(0)
+
+        for i in range (0,9):
+            for j in range (0,9):
+                self.tablero.append(self.celda[i][j].valorCorrecto)
+        self.CasillasVisibles(1)
 
 
 
-
-    def generarSolucionRandom(self):
-        resolver=False
+    def CasillasVisibles(self, dificultad):
+        lista=[Celda(0,0,"") for i in range (40) ]
+        mov_posibles=[[False for i in range(1,10)] for j in range(0,40)]
+        n=40
         random.seed(0)
-        num_de_Random = 25
-        h=0
         j=0
-        while resolver!=True:
-            while j<num_de_Random:
+        contador=0
+        contador_golbal=0
+        SeCumple=False
+        while SeCumple!=True:
+            while j<n:
                 x=random.randint(0,8)
                 y=random.randint(0,8)
-                v=random.randint(1,9)
-                print(x,y,v)
-                if self.MovimientoValido(x, y, v):
-                    self.SetValor(x, y, v)
+                if self.tablero[(x*9)+y]!=0:
+                    lista[j].columna=y
+                    lista[j].fila=x
+                    lista[j].valorCorrecto=self.tablero[(x*9)+y]
+                    self.tablero[(x*9)+y]=0
                     j+=1
-            resolver=self.resolverTablero()
+            for pos in range (0,40):
+                for v in range(1,9):
+                    mov_posibles[pos][v]=self.MovimientoValido(lista[pos].fila,lista[pos].columna,v)
+            for pos in range (0,40):
 
-    def resolverTablero(self):
-        resuelto = False
-        esvalido=self.esValido()
-        if esvalido:
-            resuelto= self.BackTrackSolucion(0,0)
-        return resuelto
+                for v in range(1,9):
+                    if mov_posibles[pos][v]:
+                        contador+=1
 
-    def esValido(self):
-        return self.ValidarVertical() & self.ValidarHorizontal() & self.ValidarBloque()
-
-    def ValidarVertical(self):
-        elementosVerticales=[]
-        valido = True
-        for j in range (10):
-            elementosVerticales.insert(j,False)
-        for i in range (9):
-            if valido==False:
-                break
+                if dificultad==1:
+                    if contador>0 & contador<4:
+                        contador_golbal+=1
+                if dificultad==2:
+                    if contador>3 & contador<6:
+                        contador_golbal+=1
+                if dificultad==3:
+                    if contador>3 & contador<8:
+                        contador_golbal+=1
+                contador=0
+            if contador_golbal>35:
+                SeCumple=True
             else:
-                for j in range(9):
-                    if valido:
-                        curElem = self.tablero[(i*9)+j]
-                        if elementosVerticales[curElem]:
-                            valido=False
-                        else:
-                            valido=True
-                        if (curElem != 0):
-                            elementosVerticales[curElem] = True
-                    else:
-                        break
-                    for k in range(10):
-                        elementosVerticales[k] = False
-        return valido
+                SeCumple=False
+        for i in range (40):
+            self.tablero[(lista[i].fila*9) +lista[i].columna]=lista[i].valorCorrecto
+            self.visibles[lista[i].fila*9 +lista[i].columna]=False
 
-    def ValidarHorizontal(self):
-        elementosVerticales=[]
-        valido = True
-        for j in range (10):
-            elementosVerticales.insert(j,False)
-        for i in range (9):
-            if valido==False:
-                break
+
+
+
+
+
+    def validateCellAtPointerWithNumber(self,row, column, value, counter):
+        i, j = 0, 0
+
+        while i < 9:
+            while j < 9:
+                if(i == row or j == column or self.celda[i][j].getCuadricula() == self.celda[row][column].getCuadricula()):
+                    if(self.celda[i][j].getValorCorrecto()):
+                        if(value == self.celda[i][j].getValorCorrecto() and not(i == row and j == column - counter)):
+                            return False
+
+                j += 1
+            j = 0
+            i += 1
+        return True
+
+
+    def numberAssigner(self,counter, row, column, value):
+        if (counter > column):
+            return False
+        if((not counter and self.validateCellAtPointerWithNumber(row, column, value, 0)) or (counter and self.validateCellAtPointerWithNumber(row, column, self.celda[row][column - counter].getValorCorrecto(), counter) and self.validateCellAtPointerWithNumber(row, column - counter, value, 0))):
+            if(not counter):
+                self.celda[row][column].setValorCorrecto(value)
             else:
-                for j in range(9):
-                    if valido:
-                        curElem = self.tablero[(j*9)+i]
-                        if elementosVerticales[curElem]:
-                            valido=False
-                        else:
-                            valido=True
-                        if (curElem != 0):
-                            elementosVerticales[curElem] = True
-                    else:
-                        break
-                    for k in range(10):
-                        elementosVerticales[k] = False
-        return valido
-
-    def ValidarBloque(self):
-        elementosBloque=[]
-        startX=0
-        startY=0
-        i=0
-        j=0
-        valido = True
-        for j in range(10):
-            elementosBloque.insert(j,False)
-        while startX<9 & valido:
-            while startY<9 & valido:
-                while valido &( i % 3 != 0 | i == startX):
-                    while valido &( j % 3 != 0 | j == startY):
-                        curElem = self.tablero[(j*9)+i]
-                        if elementosBloque[curElem]:
-                            valido=False
-                        else:
-                            valido=True
-                        if (curElem != 0):
-                            elementosBloque[curElem] = True
-                        j+=1
-                    i+=1
-                for k in range(10):
-                    elementosBloque[k] = False
-                startY+=3
-            startX += 3
-        return valido;
+                self.celda[row][column].setValorCorrecto(self.celda[row][column - counter].getValorCorrecto())
+                self.celda[row][column - counter].setValorCorrecto(value)
+            return True
+        else:
+            counter += 1
+            return self.numberAssigner(counter, row, column, value)
 
 
-    def escanearSolucion(self):
-        encontrado = self.esValido()
-        numPossible=0
-        #possibleSolutions=[]*9[]*9[]*10
-        possibleSolutions= [[ [ False for i in range(0,10) ] for j in range(0,9) ]for k in range(0,9)]
-        for y in range (9):
-            for x in range(9):
-                for v in range(1,10):
-                    possibleSolutions[y][x][v] = True
-        completo=self.esCompleto()
-        while completo==False & encontrado:
-            encontrado = False
-            #for y in range(9)
-            y=0
-            for x in range(9):
-                numPossible = 0
-                for v in range(1,10):
-                    if (possibleSolutions[x][y][v]):
-                        possibleSolutions[x][y][v] = self.MovimientoValido(x, y, v)
-                        if possibleSolutions[x][y][v]:
-                             numPossible+=1
+    def sudokuGenerator(self,row):
+        if (row > 8):
+            return
+        i, j = 0, 0
+        digitArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        rowArray = [0] * 9
+        random.shuffle(digitArray)
 
-                if (numPossible == 1):
-                    for v in range(1,10):
-                        if(possibleSolutions[x][y][v]):
-                            self.SetValor(x, y, v)
-                        encontrado = True
-            completo=self.esCompleto()
-        return self.esCompleto();
 
-    def esCompleto(self):
-        lleno = True
-        i=0
-        j=0
-        while lleno & i<9:
-            while lleno & j<9:
-                valor=self.GetValor(i,j)
-                if  valor!=0:
-                    lleno=True
-                else:
-                    lleno=False
-                    return lleno
-                j+=1
-            i+=1
-        return lleno
+        flag = False
+        while i < 9:
+            while j < 9:
+                flag = False
+                if(digitArray[j] >= 1):
+                    if(self.numberAssigner(0, row, i, digitArray[j])):
+                        digitArray[j] = -1
+                        flag = True
 
-    def BackTrackSolucion(self,startx,starty):
-        solved = self.esCompleto();
-        foundzero = False;
-        e=1
-        x=startx
-        y=starty
-        while foundzero==False and solved==False  and y < 9:
-            #print ("while y "+str(y))
-            while foundzero==False and (x <9):
-                #print ("while x "+str(x))
-                foundzero = (self.tablero[(x*9)+y] == 0)
-                x+=1
-            startx=0
-            y+=1
-        x-=1
-        y-=1
-        if (solved==False & foundzero):
-            while solved==False and e<10:
-                #print ("while e "+str(e))
-                #try:
-                if (self.MovimientoValido(x, y, e)):
-                    self.SetValor(x, y, e)
-                    if (self.BackTrackSolucion(x, y+1)):
-                        solved = True;
-                    else:
-                        self.tablero[x*9+y]
-                #except IndexError as error:
-                    #print(error)# or pass, do nothing just ignore that row...
-                e+=1
-        return solved;
+                if(flag):
+                    break
+                j += 1
+            j = 0
+            i += 1
 
-    def SetValor(self,x,y,v ):
-        self.tablero[(x*9)+y]=v;
+        for index in range(9):
+            rowArray[index] = self.celda[row][index].getValorCorrecto()
+
+        self.sudokuGenerator(row + 1)
+
+
+    def checkSudokuForZero(self):
+        i, j = 0, 0
+
+        while i < 9:
+            while j < 9:
+                if (not self.celda[i][j].getValorCorrecto()):
+                    return False
+                j += 1
+            j = 0
+            i += 1
+        return True
 
     def MovimientoValido(self,x, y, v):
         horizontal=self.ChequearHorizontal(x, y, v)
@@ -204,6 +181,7 @@ class Generador:
             return True;
         else:
             return False;
+
     def GetValor(self,x,y):
         #valor=0
         #print("len tablero "+str(len(self.tablero))+" x*9+y " +str(x*9+y))
@@ -233,19 +211,6 @@ class Generador:
         return valido
 
     def ChequearVertical(self,x, y, v):
-        #valido=False
-        #i=0
-        #if self.tablero[(x*9)+y-2] == 0:
-            #valido=True
-        #else:
-            #valido=False
-        #while i<9:
-            #if self.GetValor(x,i) == v:
-                #valido=False
-                #break
-            #else:
-                #valido=True
-            #i+=1
         valido = self.tablero[x*9+y]==0
         for i in range(9):
             if valido:
@@ -274,8 +239,28 @@ class Generador:
                 break
         return valido
 
+    def wipeSudokuClean(self):
+        i, j = 0, 0
+
+        while i < 9:
+            while j < 9:
+                self.celda[i][j].setText("")
+                self.celda[i][j].setValorCorrecto(0)
+                self.celda[i][j].setValor(0)
+                j += 1
+            j = 0
+            i += 1
+
+
+
+
 if __name__ == '__main__':
-    Tablero=Generador()
-    #Tablero.__init__()
-    for i in range (80):
-        print(Tablero.tablero[i])
+    #Tablero=Generador(1)
+    #Tab=Generador(2)
+    Ta=Generador(3)
+
+
+    for i in range (9):
+        for j in range(9):
+            print(Ta.tablero[(i*9)+j],(i*9)+j)
+        print("\n")
